de62b9782313b7ae0ea2cefc5197a95335b6c888
_ñ <knaka0209@gmail.com>
2024-05-11 19:09:15 +0900

    up, 0511a6

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
 
 import express from 'express';
-import cors from 'cors';
-import { renderToString } from 'react-dom/server';
 const app = express();
 //
-import Top from './pages/App';
+import { htmlSend } from './lib/RenderUtil'
+import TopRender from './pages/App';
 //
-//
-app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 console.log("env=", process.env.NODE_ENV)
 //
 const errorObj = {ret: "NG", messase: "Error"};
-
-// route
 //SPA
-app.get('/*', (req: any, res: any) => {
+app.get('/*', async (req: any, res: any) => {
   try {
-    res.send(renderToString(Top()));
+    const rendered = await TopRender()
+    res.send(htmlSend(rendered.html));
   } catch (error) {
+    console.error(error);
     res.sendStatus(500);
   }
 });
@@ -31,5 +28,3 @@ app.listen({ port: PORT }, () => {
   console.log(`Server ready at http://localhost:${PORT}`);
 });
 console.log('start');
-
-//export const viteNodeApp = app;
