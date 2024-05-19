735cc8328cc44cf5ea2db5dbb8926a660797c808
_ñ <knaka0209@gmail.com>
2024-05-20 06:20:44 +0900

    up, 0520a4

---
+++ sample/zod_test/src/client/Confirm.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+import type { ComponentEvents } from 'svelte';
+import type { SvelteHTMLElements  } from 'svelte/elements';
+type InputOnChangeEvent = SvelteHTMLElements['input']['on:change']
+
+import HttpCommon from './lib/HttpCommon';
+import CrudIndex from './Zod/CrudIndex';
+import Crud from './Zod/Crud';
+import Validate from './Zod/Validate';
+import DialogBox from '../components/DialogBox.svelte'
+import ErrorDialogBox from '../components/ErrorDialogBox.svelte'
+import ConfirmDialog from '../components/ConfirmDialog.svelte'
+//
+//export let errors = {};
+//
+const testProc = async function(){
+  const dlg = document.getElementById('confirmDialog');
+  if(dlg) {
+    //@ts-ignore
+    dlg.showModal();
+  }
+}
+//
+const cbFunc = async function(){
+  console.log("#cbFunc");
+  alert("parent.cbFunc");
+  const dlg = document.getElementById('confirmDialog');
+  if(dlg) {
+    //@ts-ignore
+    dlg.close();
+  }
+}
+</script>
+
+<!-- -->
+<div class="container mx-auto my-2 px-8 bg-white">
+  <h1 class="text-4xl font-bold">Confirm.svelte!</h1>
+  <hr class="my-2" />
+  <button on:click={() => testProc()} class="btn-purple">Test
+  </button>
+  <!-- dialog -->
+  <ConfirmDialog message={`OK? next`} cbFunction={cbFunc}/>
+</div>
+
+<style>
+</style>
+
+<!--
+-->
---
+++ sample/zod_test/src/components/ConfirmDialog.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+export let message: string= "", cbFunction:any;
+//console.log("message=", message);
+//
+const closeButton = function(){
+  const dlg = document.getElementById('confirmDialog');
+  if(dlg) {
+    //@ts-ignore
+    dlg.close();
+  }
+}
+const okFunc = async function(){
+  console.log("#okFunc");
+  cbFunction();
+}
+</script>
+
+<!-- MarkUp -->
+<dialog id="confirmDialog" class="dialog">
+  <div class="bg-white px-8 pt-3 pb-3 dialog_body_wrap">
+    <p class="text-3xl font-bold text-gay-900">{message}</p>
+    <hr class="my-3" />
+    <button 
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-8 rounded"
+    type="submit" on:click={() => okFunc()}>OK</button>
+    <button 
+    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-4 border border-blue-500 hover:border-transparent rounded"
+    type="submit" on:click={() => closeButton()}>Cancel</button>
+  </div>
+</dialog>
+
+<!--
+-->
